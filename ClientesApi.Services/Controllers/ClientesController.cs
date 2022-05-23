3599ebef83a8d4e174f963f097eee255ac9f4ee9
gilson-csharp <gilson_st@yahoo.com.br>
2022-05-23 12:05:01 -0300

    Estrutura inicial do controlador

---
+++ ClientesApi.Services/Controllers/ClientesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientesApi.Services.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{idCliente}")]
+        public IActionResult Delete(Guid idCliente)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{idCliente}")]
+        public IActionResult GetAll(Guid idCliente)
+        {
+            return Ok();
+        }
+    }
+}
